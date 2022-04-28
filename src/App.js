e612fd74667882966c5ca5f47a7dd947dd12bdba
its-kayes <kayes.ek8@gmail.com>
2022-04-29 00:20:47 +0600

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import About from './components/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1> 
+    <div className="App"> 
+      <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}> </Route>
+          <Route path='/home' element={<Home></Home>}>  </Route>
+          <Route path='/about' element={<About></About>}> </Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            this is About Part
+        </div>
+    );
+};
+
+export default About;
