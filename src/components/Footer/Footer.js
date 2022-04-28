4b2e36f42d0c2fe1dd4a89cf5f36333001664d70
its-kayes <kayes.ek8@gmail.com>
2022-04-29 03:06:37 +0600

    Footer Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,35 +2,34 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-20'>
+<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
             <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
                 <div class="sm:flex sm:items-center sm:justify-between">
-                            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> Clothing Warehouse </span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> Clothing Warehouse </span>
                     {/* <a href="https://flowbite.com" class="flex items-center mb-4 sm:mb-0">
                         <img src="/docs/images/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
                     </a> */}
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">  </span>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+
+                    <ul class="flex flex-wrap items-center mb-6 text-xl text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                            <a href="#" class="mr-4 hover:underline md:mr-6 "><i class="fa-brands fa-linkedin"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                            <a href="#" class="mr-4 hover:underline md:mr-6"><i class="fa-brands fa-github"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                            <a href="#" class="mr-4 hover:underline md:mr-6 "><i class="fa-brands fa-facebook"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="hover:underline">Contact</a>
+                            <a href="#" class="hover:underline"><i class="fa-brands fa-instagram"></i></a>
                         </li>
                     </ul>
                 </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> This Website is Design And Deploy by Kayes for Educational Purpose
-                    </span>
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 Copyright. All Rights Reserved.
-                    </span>
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> This Website is Design And Deploy by Kayes for Educational Purpose
+                </span>
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Copyright 2022</span>
             </footer>
 
         </div>
