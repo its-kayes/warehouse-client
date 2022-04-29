877a572e4887f851534c14023b82b26ea4490e3c
its-kayes <kayes.ek8@gmail.com>
2022-04-29 17:14:55 +0600

    Login And Social done

--- src/components/GetIn/Register.js
+++ src/components/GetIn/Register.js
@@ -2,6 +2,7 @@ import { async } from '@firebase/util';
 import React from 'react';
 import {useCreateUserWithEmailAndPassword, createUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init';
+import Social from '../Social/Social';
 const Register = () => {
 
     const [
@@ -39,6 +40,7 @@ const Register = () => {
                     <input type="submit" value="Login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
                 </form>
             </div>
+            <Social> </Social>
         </div>
     );
 };
