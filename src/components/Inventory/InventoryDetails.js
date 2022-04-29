3d0a5a0ee56d29e22b2436a8ab1ad6ac75e8288c
its-kayes <kayes.ek8@gmail.com>
2022-04-30 03:32:58 +0600

    50% Done Inventory Page

--- src/components/Inventory/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails.js
@@ -16,12 +16,9 @@ const InventoryDetails = () => {
     // useEffect()
     console.log(peritem?.userImg);
     return (
-        <div>
-            <h1> this is inventory item {id} </h1>
-            <h2> Item Name:- {peritem?.name} </h2>
-            {/* <img src={peritem?.userImg} alt="" /> */}
-            <div>
+        <div className='flex justify-around mt-14'>
 
+            <div className=''>
                 <div className="grid-cols-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <img className="rounded-t-lg" src={peritem?.userImg} alt="" />
@@ -37,9 +34,29 @@ const InventoryDetails = () => {
 
 
                         <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Stock Update
+                            Delivered
                             <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </a>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+            
+            <div className='items-center mt-32'>
+                <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex flex-col items-center pb-10 px-4 pt-6">
+                        <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src={peritem?.userImg} alt="Bonnie image" />
+                        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{peritem?.name}</h5>
+                        <span class="text-sm text-gray-500 dark:text-gray-400">How many product do you want to add ?</span>
+                        <div class="flex mt-4 space-x-3 lg:mt-6">
+                            {/* <a href="#" class="">Message</a> */}
+                            <input type="number" name="number" id="" className="inline-flex items-center w-24 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700" />
+                            <button class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+                        </div>
                     </div>
                 </div>
 
