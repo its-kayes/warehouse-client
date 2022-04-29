e36a15583e5e755345938e3175119f64bf664fbb
its-kayes <kayes.ek8@gmail.com>
2022-04-29 17:26:15 +0600

    Login/register route change

--- src/components/GetIn/Register.js
+++ src/components/GetIn/Register.js
@@ -1,6 +1,7 @@
 import { async } from '@firebase/util';
 import React from 'react';
 import {useCreateUserWithEmailAndPassword, createUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Social from '../Social/Social';
 const Register = () => {
@@ -36,6 +37,7 @@ const Register = () => {
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
                         <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                     </div>
+                    <p className='mb-6'>Have a account ? <Link to="/login"> <span className=' text-blue-700'>Please Login -{">"}</span> </Link> </p>
                     {/* <button type="submit" >Login </button> */}
                     <input type="submit" value="Login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
                 </form>
