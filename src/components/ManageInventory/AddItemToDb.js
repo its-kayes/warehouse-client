2a6a7c7785eca93353becba55dd6516bf9bf3418
its-kayes <kayes.ek8@gmail.com>
2022-05-01 14:57:25 +0600

    Alert Add to Add Item Page

--- src/components/ManageInventory/AddItemToDb.js
+++ src/components/ManageInventory/AddItemToDb.js
@@ -22,7 +22,10 @@ const AddItemToDb = () => {
             body: JSON.stringify(newItem)
         })
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data);
+                alert(" Your Product Successfully Added :3 "); 
+            })
             .catch((error) => {
                 console.error('Error:', error);
             });
@@ -58,7 +61,7 @@ const AddItemToDb = () => {
                         <label htmlFor="body" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Link of your product Image </label>
                         <input type="text" name="img" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Link of your product Image" required="" />
                     </div>
-                    
+
                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                 </form>
 
