2658b0e4ef7d130a7c07c7ea554420f5d733bf56
its-kayes <kayes.ek8@gmail.com>
2022-05-05 01:41:45 +0600

    My Items Get Done

--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyItems = () => {
+    let [myItems, setMyItems] = useState([]);
+    let [user] = useAuthState(auth);
+
+    let email = user.email;
+
+    useEffect(() => {
+        let url = `http://localhost:5000/myitems?email=${email}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setMyItems(data));
+    }, [user])
+
+
     return (
         <div>
             
-            <h1> this is my items part </h1>
+            <h1> this is my items part {myItems.length} </h1>
 
         </div>
     );
