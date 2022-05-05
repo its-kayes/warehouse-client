b4067e85fedb355d533606845c1be5f57aae9f90
its-kayes <kayes.ek8@gmail.com>
2022-05-06 01:33:09 +0600

    Blogs Create

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Inventory from './components/Inventory/Inventory';
 import InventoryDetails from './components/Inventory/InventoryDetails';
 import ManageInventory from './components/ManageInventory/ManageInventory';
 import AddItemToDb from './components/ManageInventory/AddItemToDb';
+import Blogs from './components/About/Blogs';
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
           </RequireAuth>}> </Route>
 
         <Route path='/register' element={<Register></Register>}> </Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='*' element={<NotFound />}> </Route>
       </Routes>
       <Footer></Footer>
---
+++ src/components/About/Blogs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1> Its blogs part </h1>
+            <p>
+                <p>
+                    Different Betw
+                </p>
+                <p>
+
+                </p>
+            </p>
+            <p>
+
+            </p>
+            <p>
+
+            </p>
+        </div>
+    );
+};
+
+export default Blogs;
