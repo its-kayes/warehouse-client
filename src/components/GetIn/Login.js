4e9a90b94e48715b3a9e14d4b961ff702e68a7d4
its-kayes <kayes.ek8@gmail.com>
2022-05-07 12:49:33 +0600

    Login Error Show

--- src/components/GetIn/Login.js
+++ src/components/GetIn/Login.js
@@ -4,6 +4,8 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Social from '../Social/Social';
 
+
+let errorElement;
 const Login = () => {
 
     let navigate = useNavigate();
@@ -34,27 +36,13 @@ const Login = () => {
     }
 
 
+    if (error) {
+        errorElement = <p className='text-center bg-red-700 rounded-full mt-5 text-white p-3'>  Error Login </p>
+    }
+
 
     return (
         <div >
-            {/* <h1> This is login Page </h1> */}
-            {/* <div>
-                <form onSubmit={loginSubmit}>
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required="" />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-                        <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-                    </div>
-                    <p className='mb-6'>New to on it? <Link to="/register"> <span className=' text-blue-700'>Please Register -{">"}</span> </Link> </p>
-                    <input type="submit" value="Login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
-                </form>
-            </div> */}
-
-
-
             <div className='flex justify-center'>
 
                 <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ">
@@ -83,6 +71,9 @@ const Login = () => {
                             <Link to="/register"> <span className='text-blue-700 hover:underline dark:text-blue-500'>Create account</span> </Link>
                         </div>
                     </form>
+                    <div className=''> 
+                        {errorElement}
+                    </div>
                     <p className='mt-5'> ________________________________________ </p>
                     <Social> </Social>
                 </div>
--- src/components/GetIn/Register.js
+++ src/components/GetIn/Register.js
@@ -4,6 +4,12 @@ import { useCreateUserWithEmailAndPassword, createUserWithEmailAndPassword } fro
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Social from '../Social/Social';
+
+
+
+
+let errorElement;
+
 const Register = () => {
 
     const [
@@ -14,8 +20,6 @@ const Register = () => {
     ] = useCreateUserWithEmailAndPassword(auth);
 
 
-
-
     let registerSubmit = async event => {
         event.preventDefault();
         const email = event.target.email.value;
@@ -24,37 +28,14 @@ const Register = () => {
         await createUserWithEmailAndPassword(email, password);
     }
 
+
+    if (error) {
+        errorElement = <p className='text-center bg-red-700 rounded-full mt-5 text-white p-3'>  Error Login </p>
+    }
+
     return (
         <div>
-            {/* <h2> This is register Part </h2> */}
-            {/* <div>
-                <form onSubmit={registerSubmit}>
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required="" />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-                        <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-                    </div>
-                    <p className='mb-6'>Have a account ? <Link to="/login"> <span className=' text-blue-700'>Please Login -{">"}</span> </Link> </p>
-                    <input type="submit" value="Login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
-                </form>
-            </div>
-            <Social> </Social> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
             <div className='flex justify-center'>
 
                 <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ">
@@ -82,6 +63,9 @@ const Register = () => {
                             <Link to="/login"> <span className='text-blue-700 hover:underline dark:text-blue-500'>Please Login</span> </Link>
                         </div>
                     </form>
+                    <div className=''> 
+                        {errorElement}
+                    </div>
                     <p className='mt-5'> ________________________________________ </p>
                     <Social> </Social>
                 </div>
